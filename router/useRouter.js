744fe5f3278352118ccd6c55e45d31b5df7f1637
Vito Millozza <v.millozza@gmail.com>
2023-08-21 09:48:50 +0200

    Added mongoose

--- router/useRouter.js
+++ router/useRouter.js
@@ -22,6 +22,28 @@ router.get('/:id', (req, res, next) => {
     });
 
 });
+router.put('/:id', (req, res, next) => {
+    res.status(200).json({
+        message: 'Succefull - PUT by Id',
+        metadata: {
+            id: req.params.id,
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+
+});
+router.delete('/:id', (req, res, next) => {
+    res.status(200).json({
+        message: 'Succefull - DELETE by Id',
+        metadata: {
+            id: req.params.id,
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+
+});
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'Succefull - POST',
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const app = require('./app/app');
 require('dotenv').config();
 const server = http.createServer(app
-).listen(process.env.PORT,()=>{
+).listen(process.env.PORT, () => {
     console.log(`Server in esecuzione su :${process.env.PORT}`);
 });
