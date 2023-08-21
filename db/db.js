6a421e92f2e007cd8d1de51b91d81a9bcc857fcc
Vito Millozza <v.millozza@gmail.com>
2023-08-21 10:51:25 +0200

    Db operation

--- db/db.js
+++ db/db.js
@@ -1,9 +1,31 @@
 const { default: mongoose } = require('mongoose');
-
+const User = require("../models/userModel")
 require('dotenv').config();
 
-const connect = async ()=>{
-    await mongoose.connect(process.env.mongo);
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.mongo);
+        console.log('Connected to MongoDB');
+    }
+    catch (err) {
+        console.error('Error connecting to MongoDB', err);
+    }
+}
+
+const disconnect =async()=>{
+    try{
+        await mongoose.connection.close();
+    }
+    catch (err) {
+        console.error('Error to disconnecting to MongoDB', err);
+    }
+}
+const findUser =async(obj)=>{
+    User.findOne(obj)
+}
+const saveUser =async(newuser)=>{
+    await  newuser.save();
+    
 }
 //{connect}: Questa è una sintassi di inizializzazione dell'oggetto ES6. È equivalente a scrivere {connect: connect}. Ciò significa che stai esportando un oggetto con una proprietà chiamata connect, il cui valore è la variabile/funzione connect definita nel modulo.
-module.exports = {connect};
+module.exports = { connect,disconnect,findUser,saveUser };
--- router/useRouter.js
+++ router/useRouter.js
@@ -54,4 +54,12 @@ router.post('/', (req, res, next) => {
     });
 
 });
+router.post('/register', (req, res, next) => {
+   
+
+});
+router.post('/login', (req, res, next) => {
+    
+
+});
 module.exports = router;
